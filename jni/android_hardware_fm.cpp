99efaf6e0c24fddb87b599ce304cd2156b2ccfe0
Ricardo Cerqueira <ricardo@cyngn.com>
2014-11-04 15:14:08 +0000

    jni: Skip loading FM firmware if requested
    Not all boards require a firmware download for FM. Set
    TARGET_QCOM_NO_FM_FIRMWARE if the device doesn't require
    a fw image to run FM
    
    Change-Id: If18650af934f840a54f53fa9474a3341df0e3e68

--- jni/android_hardware_fm.cpp
+++ jni/android_hardware_fm.cpp
@@ -647,6 +647,7 @@ static jint android_hardware_fmradio_FmReceiverJNI_acquireFdNative
        /*Set the mode for soc downloader*/
        property_set("hw.fm.mode", "normal");
        /* Need to clear the hw.fm.init firstly */
+#ifndef QCOM_NO_FM_FIRMWARE
        property_set("hw.fm.init", "0");
        property_set("ctl.start", "fm_dl");
        sched_yield();
@@ -667,6 +668,9 @@ static jint android_hardware_fmradio_FmReceiverJNI_acquireFdNative
          return FM_JNI_FAILURE;
        }
     }
+#else
+    usleep(WAIT_TIMEOUT);
+#endif
     return fd;
 }
 
@@ -1125,6 +1129,7 @@ static jint android_hardware_fmradio_FmReceiverJNI_setNotchFilterNative(JNIEnv *
        else
           property_set("hw.fm.mode", "wa_disable");
 
+#ifndef QCOM_NO_FM_FIRMWARE
        property_set("ctl.start", "fm_dl");
        sched_yield();
        for(i=0; i<10; i++) {
@@ -1137,6 +1142,9 @@ static jint android_hardware_fmradio_FmReceiverJNI_setNotchFilterNative(JNIEnv *
           }
        }
        ALOGE("init_success:%d after %f seconds \n", init_success, 0.2*i);
+#else
+    usleep(WAIT_TIMEOUT);
+#endif
 
        property_get("notch.value", notch, NULL);
        ALOGE("Notch = %s",notch);
